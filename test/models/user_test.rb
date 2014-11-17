05a2a5a2b2ca63cc80db99ae3d5a226c28306c87
zibs <elizibin@gmail.com>
2014-11-16 20:04:17 -0800

    Finish notes on Chp6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
 
 
   def setup
-   @user = User.new(name: "Example User", email: "user@example.com",
+   @user = User.new(name: "Exa", email: "user@example.com",
    					password: "foobar", password_confirmation: "foobar"	)
   end
 
