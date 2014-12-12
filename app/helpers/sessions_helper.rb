435e810d4b0cb29ad9dcaac34a186414213479d5
zibs <elizibin@gmail.com>
2014-12-12 09:42:20 -0800

    Finish cahp9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,11 @@ module SessionsHelper
 		cookies.permanent[:remember_token] = user.remember_token
 	end
 
+
+	 def current_user?(user)
+    user == current_user
+  end
+
 	
  # Returns the user corresponding to the remember token cookie.
   def current_user
@@ -42,6 +47,17 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
+	def redirect_back_or(default) 
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end
+	
 
 
 
